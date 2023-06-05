8070a767e9085444f167152a989ff59b446fbf78
Chelo <carlosmarcelotorresvargas@gmail.com>
2023-06-05 18:49:07 -0400

    fix deploy 1

--- app.js
+++ app.js
@@ -11,12 +11,7 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(
-	cors({
-		origin: process.env.HOST_FRONT || 'http://127.0.0.1:5174',
-		credentials: true,
-	})
-);
+app.use(cors());
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
