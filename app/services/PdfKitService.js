ba8ebbacc4525c0dc14920710aa51bd60e2eadba
Chelo <carlosmarcelotorresvargas@gmail.com>
2023-06-06 00:23:41 -0400

    fix: deploy with front

--- app/services/PdfKitService.js
+++ app/services/PdfKitService.js
@@ -30,7 +30,10 @@ const crearPdf = async (
 
 	content += buildDataTable(data);
 
-	const browser = await puppeteer.launch({headless: 'new'});
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
 	const page = await browser.newPage();
 
 	await page.setContent(content);
